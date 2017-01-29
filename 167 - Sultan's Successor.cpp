7b2b836ad163097b55fe3dcf5e40c536ef08045d
MD. Minhazul Islam <minhazmiraz@hotmail.com>
2017-01-29 16:03:04 +0600

    Update and rename 167.cpp to 167 - Sultan's Successor.cpp

--- 167 - Sultan's Successor.cpp
+++ 167 - Sultan's Successor.cpp
@@ -1,26 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-int rowFlag[10],colFlag[10],leftDiag[16],rightDiag[16];
-int maxx,flag;
-int arr[10][10];
-
+int colFlag[10],leftDiag[16],rightDiag[16];
+int arr[10][10],maxx,cnt;
+ 
 void eightQueen(int pos,int sum)
 {
     if(pos==9)
     {
         maxx=max(maxx,sum);
-        //cout<<sum<<endl;
+        cnt++;
+        //cout<<sum<<""<<cnt<<endl;
+ 
         return;
     }
     for(int i=1;i<=8;i++)
     {
-        if(!colFlag[i]&&!leftDiag[8+(pos-1)]&&!rightDiag[i+pos])
+        if(!colFlag[i]&&!leftDiag[8+(pos-i)]&&!rightDiag[i+pos])
         {
             colFlag[i]=1;
-            leftDiag[8+(pos-1)]=1;
+            leftDiag[8+(pos-i)]=1;
             rightDiag[i+pos]=1;
             sum+=arr[pos][i];
+ 
             eightQueen(pos+1,sum);
+ 
+            sum-=arr[pos][i];
             leftDiag[8+(pos-i)]=0;
             rightDiag[i+pos]=0;
             colFlag[i]=0;
@@ -33,7 +37,6 @@ int main()
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
     {
-        memset(rowFlag,0,sizeof(rowFlag));
         memset(colFlag,0,sizeof(colFlag));
         memset(leftDiag,0,sizeof(leftDiag));
         memset(rightDiag,0,sizeof(rightDiag));
@@ -42,7 +45,9 @@ int main()
             for(int k=1;k<=8;k++)
                 scanf("%d",&arr[j][k]);
         maxx=0;
+        cnt=0;
         eightQueen(1,0);
+        cout.width(5);
         cout<<maxx<<endl;
     }
 }
